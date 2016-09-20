f3c6fb1f7ab210e8d9a54f3de8a7e1dd99db4ad2
soderlind <per@soderlind.no>
2016-09-21 00:47:45 +0200

    Rename template

---
+++ templates/newsletter.php
@@ -0,0 +1,28 @@
+<body class="single " >
+	<div id="page" class="site">
+		<div class="site-inner">
+			<div id="content" class="site-content">
+				<div id="primary" class="content-area <?php /*echo esc_attr( $classes );*/ ?>">
+					<main id="main" class="site-main" role="main">
+						<article>
+							<header class="entry-header">
+								<h1 class="entry-title">Newsletter</h1>
+							</header>
+							<div class="entry-content content">
+								<p>
+								Nulla laoreet erat vitae aliquet tristique. Quisque quis suscipit enim, a elementum eros. Vestibulum quis lacus ligula. Mauris sit amet nibh tincidunt, gravida tortor ac, maximus nisi. Nunc turpis mauris, blandit vitae nibh non, blandit pharetra purus. Sed sed convallis diam. In ante nisi, pellentesque in laoreet at, fringilla ac neque.
+								</p>
+								<p>
+								Cras consequat eros lectus, ac dignissim justo cursus ac. In mi tortor, ornare eu quam a, ultrices tincidunt magna. Fusce non purus quis ex convallis vehicula. Aenean purus diam, vulputate vel pellentesque ultrices, tincidunt ac lectus. Nullam est purus, lobortis ac aliquam eget, egestas a diam. Praesent eget sapien vitae orci faucibus tempus. Ut sit amet lorem elit. Curabitur et vestibulum nibh.
+								</p>
+								<p>
+								Mauris vestibulum eget nisl sit amet gravida. Nullam in orci ut sem maximus dapibus blandit quis dolor. Sed pretium efficitur elit, eget vulputate sapien bibendum non. Proin congue posuere sagittis. Ut fermentum mauris ut gravida faucibus. Aenean a malesuada magna. Vestibulum nec viverra metus. Nunc aliquam eros orci, ut malesuada felis malesuada vitae. Phasellus vehicula non ex at scelerisque.
+								</p>
+							</div>
+						</article>
+					</main>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
