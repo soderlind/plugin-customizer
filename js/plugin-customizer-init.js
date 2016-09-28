f1f2b5df7ec6b6b44ee0c3253f91a3f04fe1f4c3
soderlind <per@soderlind.no>
2016-09-28 08:27:05 +0200

    Add update preview script

---
+++ js/plugin-customizer-init.js
@@ -0,0 +1,12 @@
+(function($) {
+    wp.customize( 'newsletter_title', function(value) {
+        value.bind(function(to) {
+            $( '#newsletter-title').html(to);
+        });
+    });
+    wp.customize('newsletter_content', function(value) {
+        value.bind(function(to) {
+            $('#newsletter-content').html(to);
+        });
+    });
+})(jQuery);
