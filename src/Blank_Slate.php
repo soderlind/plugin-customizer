924a4582e0dab9c32a6dc067ecfe737b000d339c
soderlind <per@soderlind.no>
2017-01-08 13:40:57 +0100

    Rename and move file

--- src/Blank_Slate.php
+++ src/Blank_Slate.php
@@ -5,23 +5,41 @@
  * Learn more at: https://make.xwp.co/2016/09/11/resetting-the-customizer-to-a-blank-slate/
  * Copyright (c) 2016 XWP (https://make.xwp.co/)
  */
+namespace PluginCustomizer;
 
-if ( ! class_exists( 'PluginCustomizerBlankSlate' ) ) {
-	class PluginCustomizerBlankSlate {
+if ( ! class_exists( 'PluginCustomizer\Blank_Slate' ) ) {
+	class Blank_Slate {
 
-		protected static $_instance = null;
-		private static $param_name;
+		private $slug;
+		private $plugin_url;
+		private $plugin_root;
 
-		public static function instance() {
-			if ( is_null( self::$_instance ) ) {
-				self::$_instance = new self();
-			}
-			return self::$_instance;
+		/**
+		 * Singleton from: from http://stackoverflow.com/a/15870364/1434155
+		 */
+		private static $instances = array();
+		protected function __construct() {}
+		protected function __clone() {}
+		public function __wakeup() {
+			throw new Exception( 'Cannot unserialize singleton' );
 		}
 
-		function __construct() {
-			// self::$param_name = sanitize_title( get_called_class() , 'plugin_customizer' );
+		public static function instance() {
+			$class = get_called_class(); // late-static-bound class name
+			if ( ! isset( self::$instances[ $class ] ) ) {
+				self::$instances[ $class ] = new static();
+			}
+			return self::$instances[ $class ];
+		}
 
+		function init( $slug = '', $plugin_url, $plugin_root ) {
+			// self::$param_name = sanitize_title( get_called_class() , 'plugin_customizer' );
+			if ( '' == $slug ) {
+				wp_die( 'slug missing', $title = 'BlankSlate::init' );
+			}
+			$this->slug = $slug;
+			$this->plugin_url = $plugin_url;
+			$this->plugin_root = $plugin_root;
 			add_filter( 'customize_loaded_components', function() {
 
 				$priority = 1;
@@ -49,7 +67,7 @@ if ( ! class_exists( 'PluginCustomizerBlankSlate' ) ) {
 				global $wp_customize;
 				$wp_customize->set_preview_url(
 					add_query_arg(
-						array( PluginCustomizer::$id => 'on' ),
+						array( $this->slug => 'on' ),
 						$wp_customize->get_preview_url()
 					)
 				);
@@ -57,14 +75,14 @@ if ( ! class_exists( 'PluginCustomizerBlankSlate' ) ) {
 
 			add_action( 'customize_controls_enqueue_scripts', function() {
 				$handle = 'plugin-customizer-blank-slate';
-				$src = plugins_url( 'js/plugin-customizer-blank-slate.js', __FILE__ );
+				$src = $this->plugin_url . '/src/assets/js/plugin-customizer-blank-slate.js';
 				$deps = array( 'customize-controls' );
 				$ver = false;
 				$in_footer = true;
 				wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 
 				$args = array(
-					'queryParamName' => PluginCustomizer::$id,
+					'queryParamName' => $this->slug,
 					'queryParamValue' => 'on',
 				);
 				wp_add_inline_script(
